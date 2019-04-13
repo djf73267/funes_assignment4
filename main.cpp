2b9a16844d5c737c5c6929ae2e647513bf0dfbfa
djf73267 <33610134+djf73267@users.noreply.github.com>
2019-04-12 21:24:33 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,89 +11,89 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    BinaryTree list;
+    BinaryTree tree;
     vector<int> numbers;
     ifstream inputFile(argv[1], ios::in); //allows input from a stream of argument 1
     int number = 0;
     while(inputFile >> number) { //a while loop that in adds an element to the vector as it pass through number
         numbers.push_back(number);
     }
-    for(int i = 0; i < numbers.size(); i++) { //for loop based on vector length and inserts number value into new item in binary tree
+    for(std::size_t i = 0; i < numbers.size(); i++) { //for loop based on vector length and inserts number value into new item in binary tree
         ItemType newItem(numbers[i]);
       
-        list.insert(newItem);
+        tree.insert(newItem);
     }
     
 cout << "Commands: insert (i), delete (d), retrieve (r), length (l), in-order (n),pre-order (p), post-order (o), quit (q), getSameLevelNonSiblings (c), quit (q)" << endl;
 cout << endl;
 while(true){
-    string s1;
+    string input;
     cout << "Enter a command: ";
-    getline(cin, s1); // retrieves line as string s1
-        if(s1 == "q"){ //if string value is equal to q then
+    getline(cin, input); // retrieves line as string input
+        if(input == "q"){ //if string value is equal to q then
             cout << "Quitting program.." << endl;
             exit(EXIT_SUCCESS); //quits program
         }
-        else if(s1=="i"){
+        else if(input=="i"){
             cout << "Enter a number: ";
             int val;
             cin >> val;
             cin.ignore(1,'\n'); //ignores 1 char until breakpoint new line
             ItemType item(val); //inserts value within item
-            list.insert(item); //inserts item
+            tree.insert(item); //inserts item
             cout << endl;
             cout << "In-Order: "; //prints in order
-            list.inOrder();
+            tree.inOrder();
             cout << endl;
         }
-        else if(s1=="d"){
+        else if(input=="d"){
             cout << "Item to delete: ";
             int val;
             cin >> val;
             cin.ignore(1,'\n'); //ignores 1 char until breakpoint new line
             ItemType item2(val); //inserts value within item
-            list.deleteItem(item2); //deletes item
+            tree.deleteItem(item2); //deletes item
             cout << endl;
             cout << "In-Order: ";
-            list.inOrder(); //prints in order
+            tree.inOrder(); //prints in order
             cout << endl;
         }
-        else if(s1=="r"){
+        else if(input=="r"){
             cout << "Item to be retrieved: ";
             int val;
             cin >> val;
             cin.ignore(1,'\n'); //ignores 1 char until breakpoint new line
             ItemType item2(val); //inserts value within item
             bool found = false; //establishes found is false
-            list.retrieve(item2, found); //gives paramters to retrieve item
+            tree.retrieve(item2, found); //gives paramters to retrieve item
             cout << endl;
         }
-        else if(s1=="c"){
+        else if(input=="c"){
             cout << "Item to find level for: ";
             int val;
             cin >> val;
             cin.ignore(1,'\n'); //ignores 1 char until breakpoint new line
             ItemType item3(val); //inserts value within item
-            list.getSameLevelNonSiblings(item3); //finds non siblings on the same level
+            tree.getSameLevelNonSiblings(item3); //finds non siblings on the same level
             cout << endl;
         }
-        else if(s1=="o"){
+        else if(input=="o"){
             cout << "Post-Order: ";
-            list.postOrder(); //prints post order
+            tree.postOrder(); //prints post order
             cout << endl;
         }
-        else if(s1=="p"){
+        else if(input=="p"){
             cout << "Pre-Order: ";
-            list.preOrder(); //prints pre order
+            tree.preOrder(); //prints pre order
             cout << endl;
         }
-        else if(s1=="n"){
+        else if(input=="n"){
             cout << "In-Order: "; //prints in order
-            list.inOrder();
+            tree.inOrder();
             cout << endl;
         }
-        else if(s1=="l"){
-            cout << "Length: " << list.getLength() << endl; //prints length
+        else if(input=="l"){
+            cout << "Length: " << tree.getLength() << endl; //prints length
                         
         }
         else{
