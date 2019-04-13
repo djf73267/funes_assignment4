f996d2e515cf6b66c6488b15476f7aa8b494d710
djf73267 <33610134+djf73267@users.noreply.github.com>
2019-04-12 21:07:28 -0400

    Add files via upload

--- BinaryTree.h
+++ BinaryTree.h
@@ -8,28 +8,112 @@ struct Node{
 };
 class BinaryTree{
 public:
+    /*
+     Pre-Condition: none
+     Post-Conditon: tree is initialized
+     **/
     BinaryTree();
+    /*
+     Pre-Condition: tree has been initialzied
+     Post-Conditon: all nodes are freed, root points to null length set to 0
+     **/
     ~BinaryTree();
+    /*
+     Pre-Condition: tree and parameter tree has been initialized
+     Post-Conditon: 
+     **/
     void clearAll(Node* tree);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void insert(ItemType &key);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void deleteItem(ItemType &key);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void retrieve(ItemType &item, bool &found);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void getItem(Node* node, ItemType& item, bool&found);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void putItem(ItemType item, Node*& node, bool& found);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void preOrder();
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void inOrder();
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void printInOrder(Node* root);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void printPreOrder(Node* root);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void printPostOrder(Node* root);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void getPredecessor(Node*& node, ItemType& data);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void DeleteNode(Node*& tree);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void Delete(Node*& root, ItemType& item);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void postOrder();
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     int getLength() const;
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     int findLevel(ItemType& item, Node*& tree, int level);
-    void printSameLevelNonSiblings(Node*& tree, ItemType& item, int
-                                   level, bool&found);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
+    void printSameLevelNonSiblings(Node*& tree, ItemType& item, int level, bool&found);
+    /*
+     Pre-Condition:
+     Post-Conditon:
+     **/
     void getSameLevelNonSiblings(ItemType &key);
+    
     Node *root;
 private:
     int length;
